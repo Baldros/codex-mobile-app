ed33e655538e9403788f9c78d3456dcc88ade3d4
André Amorim <amorimandre2012@gmail.com>
2026-06-25 10:44:48 -0300

    Fix mobile Jest setup after dependency updates

--- mobile/jest.config.js
+++ mobile/jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: "jest-expo",
+  moduleDirectories: ["node_modules", "<rootDir>/node_modules/expo/node_modules"],
   testMatch: ["**/test/**/*.test.ts"],
   testPathIgnorePatterns: ["/node_modules/"]
 };
